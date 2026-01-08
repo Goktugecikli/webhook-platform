61ba1c47aae1095949fbba8d5078b161725f9193
Goktugecikli <ggecikli@hotmail.com>
2026-01-09 02:42:46 +0300

    feat(infrastructure): add appdbcontext and webhook delivery mapping

--- src/WebHooks.Infrastructre/Persistence/AppDbContext.cs
+++ src/WebHooks.Infrastructre/Persistence/AppDbContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using WebHooks.Domain;
 
-namespace WebHooks.Infrastructre.Persistence
+namespace WebHooks.Infrastructre.Persistence;
+
+public class AppDbContext : DbContext
 {
-    internal class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
+    }
+
+    public DbSet<WebhookDelivery> WebhookDeliveries => Set<WebhookDelivery>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
     }
 }
--- src/WebHooks.Infrastructre/Persistence/Configurations/WebHookDeliveryConfiguration.cs
+++ src/WebHooks.Infrastructre/Persistence/Configurations/WebHookDeliveryConfiguration.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebHooks.Domain;
 
-namespace WebHooks.Infrastructre.Persistence.Configurations
+namespace WebHooks.Infrastructre.Persistence.Configurations;
+
+public class WebhookDeliveryConfiguration : IEntityTypeConfiguration<WebhookDelivery>
 {
-    internal class WebHookDeliveryConfiguration
+    public void Configure(EntityTypeBuilder<WebhookDelivery> b)
     {
+        b.ToTable("webhook_deliveries");
+
+        b.HasKey(x => x.Id);
+
+        b.Property(x => x.Provider)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        b.Property(x => x.EventType)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        b.Property(x => x.Payload)
+            .IsRequired();
+
+        b.Property(x => x.IdempotencyKey)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        b.HasIndex(x => new { x.Provider, x.IdempotencyKey })
+            .IsUnique();
+
+        b.Property(x => x.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        b.Property(x => x.RetryCount).IsRequired();
+
+        b.Property(x => x.NextRetryAt);
+
+        b.Property(x => x.LastError)
+            .HasMaxLength(2000);
+
+        b.Property(x => x.CreatedAt).IsRequired();
+        b.Property(x => x.UpdatedAt).IsRequired();
     }
 }
