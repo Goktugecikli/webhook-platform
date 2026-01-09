a7f04f4b8e069d5648b94acf9c769326141b0402
Goktugecikli <ggecikli@hotmail.com>
2026-01-09 03:16:57 +0300

    feat(api): register appdbcontext for postgres

--- src/WebHooks.Api/Program.cs
+++ src/WebHooks.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using WebHooks.Infrastructre.Persistence;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Controllers
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var cs = builder.Configuration.GetConnectionString("Default")
+         ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(o => o.UseNpgsql(cs));
+
 
 // ProblemDetails
 builder.Services.AddProblemDetails();
